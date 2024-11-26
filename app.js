932356730e3347224c8c99e140c36233f3c1062f
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-11-26 23:09:10 +0530

    updated app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const numberHours = document.querySelector(".number-hours");
+
+const numberElement = [];
+
+//create numbers hours
+for (let i = 1; i <= 12; i++) {
+  numberElement.push(`<span><p></p></span>`);
+}
